f2c026d446059eb6b2e7664d099681e5c0603fd7
Omae Masaki <104180839+masaki21@users.noreply.github.com>
2025-05-04 07:37:34 +0900

    Update bedrock-chatbot.ts

--- bin/bedrock-chatbot.ts
+++ bin/bedrock-chatbot.ts
@@ -4,12 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 import { BedrockChatbotStack } from '../lib/bedrock-chatbot-stack';
 
 const app = new cdk.App();
-const externalApiUrl = 'https://9fc8-34-125-85-61.ngrok-free.app/predict';
 new BedrockChatbotStack(app, 'BedrockChatbotStack', {
   // モデルIDをオプションで指定可能
   modelId: 'us.amazon.nova-lite-v1:0',
   //modelId: 'us.amazon.nova-micro-v1:0',
-  apiUrl: externalApiUrl,
   
   // 環境変数から取得したリージョンを使用、またはデフォルトとしてus-east-1を使用
   env: { 
